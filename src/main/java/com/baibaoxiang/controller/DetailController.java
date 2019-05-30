b5508490d7373e8ab075b462fcc953c2f74ee8ac
chenlin <269344402@qq.com>
2019-05-30 10:24:58 +0800

    调用增加阅读数的方法

--- src/main/java/com/baibaoxiang/controller/DetailController.java
+++ src/main/java/com/baibaoxiang/controller/DetailController.java
@@ -1,6 +1,7 @@
 package com.baibaoxiang.controller;
 import com.baibaoxiang.po.Article;
 import com.baibaoxiang.service.ArticleService;
+import com.baibaoxiang.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +15,14 @@ import org.springframework.web.servlet.ModelAndView;
 public class DetailController {
     @Autowired
     ArticleService articleService;
+    @Autowired
+    RedisService redisService;
     @RequestMapping(value = "/detail",method= RequestMethod.GET)
     public ModelAndView showDetail(String no) throws Exception {
         ModelAndView modelAndView = new ModelAndView("/detail");
         //根据id查文章的具体内容
         Article article = articleService.selectByPrimaryKey(no);
+        redisService.saveReadNumRedis(no);
         modelAndView.addObject("article",article);
         return modelAndView;
     }
